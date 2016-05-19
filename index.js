f991b535856ead3ca99b38a4530c8336db212e78
Daniel Pedroso <daniel.exe@gmail.com>
2016-05-19 13:53:26 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const uuid = require('node-uuid')
+
+module.exports = (options) => (req, res, next) => {
+    const headerName = (options || {}).headerName || 'X-Request-Id'
+    
+    if (req.headers[headerName]) {
+        req.id = String(req.headers[headerName]).valueOf()
+    } else {
+        req.id = String(uuid.v4()).valueOf()
+    }
+    
+    if (!res.headers[headerName]) {
+        res.setHeader(headerName, String(req.id).valueOf())
+    }
+    
+    return next()
+}
---
+++ test/specs.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const should = require('should')
+const reqid = require('../')
+const uuid = require('node-uuid')
+
+const getRes = () => {
+    return {
+        headers: {},
+        setHeader: function (name, value) {
+            this.headers[name] = value
+        }
+    }
+}
+
+const headerName = 'X-Request-Id'
+const middleware = reqid()
+
+describe("RequestID", () => {
+    it('should generate request id for new request', (done) => {
+        let req = { // req
+                headers: {},
+                id: undefined
+            },
+            res = getRes()
+        
+        middleware(req, res, () => {
+            req.should.have.property('id')
+            req.id.should.be.ok()
+            res.headers.should.have.property(headerName)
+            req.id.should.equal(res.headers[headerName])
+            done()
+        })
+    })
+    
+    it('should keep existing id', (done) => {
+        let id = uuid.v4()
+        let req = {
+            headers: { }
+        }
+        req.headers[headerName] = String(id).valueOf()
+        
+        let res = getRes()
+        
+        middleware(req, res, () => {
+            req.should.have.property('id', id)
+            res.headers.should.have.property(headerName, id)
+            
+            done()
+        })
+    })
+    
+    it('should not override response header', (done) => {
+        const resId = 'a;lsdkfjqwoerht'
+        
+        let id = uuid.v4()
+        let req = {
+            headers: { }
+        }
+        req.headers[headerName] = String(id).valueOf()
+        
+        let res = getRes()
+        res.headers[headerName] = resId
+        
+        middleware(req, res, () => {
+            req.should.have.property('id', id)
+            res.headers.should.have.property(headerName, resId)
+            
+            done()
+        })
+    })
+    
+    it('should work with other headerNames', (done) => {
+        let middleware = reqid({ 'headerName': 'shabushabu' })
+        let req = { headers: {} }
+        let res = getRes()
+        
+        middleware(req, res, () => {
+            req.should.have.property('id')
+            res.headers.should.have.property('shabushabu', req.id)
+            
+            done()
+        })
+    })
+})
